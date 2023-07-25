d7b4f82d0fdf1ecb0a775bcb899fc347248d6a0a
Ratan01 <ratanrudra010@gmail.com>
2023-07-25 13:21:44 +0530

    subnet prefixs updated

--- src/main.tf
+++ src/main.tf
@@ -37,7 +37,7 @@ resource "azurerm_subnet" "internal" {
   name                 = "RG-Terraform-snet-in"
   virtual_network_name = "${azurerm_virtual_network.main.name}"
   resource_group_name  = "${azurerm_resource_group.main.name}"
-  address_prefix       = ["10.100.2.0/24"]
+  address_prefixes       = ["10.100.2.0/24"]
 }
 
 # Create a Network Security Group with some rules
