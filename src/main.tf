2881de8bf7bbe35991d6c0fdbe3952b60358bb9c
Ratan01 <ratanrudra010@gmail.com>
2023-08-02 12:29:05 +0530

    main.tf updated

--- src/main.tf
+++ src/main.tf
@@ -9,40 +9,32 @@ provider "azurerm" {
 }
 
 # Locate the existing custom/golden image
-data "azurerm_image" "img" {
+data "azurerm_image" "search" {
   name                = "ratan_vm_image_dn"
   resource_group_name = "ratan"
 }
 
 output "image_id" {
-  value = "/subscriptions/fab6bd82-e9fb-4229-91d4-476d41c138fb/resourceGroups/ratan-EASTUS-SPT-PLATFORM/providers/Microsoft.Compute/images/ratan_vm_image_dn"
+  value = "/subscriptions/fab6bd82-e9fb-4229-91d4-476d41c138fb/resourceGroups/RATAN-EASTUS-SPT-PLATFORM/providers/Microsoft.Compute/images/ratan_vm_image_dn"
 }
 
 # Create a Resource Group for the new Virtual Machine.
 resource "azurerm_resource_group" "main" {
   name     = "ratan"
-  location = "EAST US"
-}
-
-# Create a Virtual Network within the Resource Group
-resource "azurerm_virtual_network" "main" {
-  name                = "RG-Vnet"
-  address_space       = ["10.100.0.0/16"]
-  resource_group_name = "${azurerm_resource_group.main.name}"
-  location            = "${azurerm_resource_group.main.location}"
+  location = "eastus"
 }
 
 # Create a Subnet within the Virtual Network
 resource "azurerm_subnet" "internal" {
   name                 = "RG-Terraform-snet-in"
-  virtual_network_name = "${azurerm_virtual_network.main.name}"
+  virtual_network_name = "RG-OPT-QA-Vnet"
   resource_group_name  = "${azurerm_resource_group.main.name}"
-  address_prefixes       = ["10.100.2.0/24"]
+  address_prefix       = "10.100.2.0/24"
 }
 
 # Create a Network Security Group with some rules
 resource "azurerm_network_security_group" "main" {
-  name                = "RG-QA-Test-Web-NSG"
+  name                = "RG-QA-Test-Dev-NSG"
   location            = "${azurerm_resource_group.main.location}"
   resource_group_name = "${azurerm_resource_group.main.name}"
 
@@ -62,22 +54,22 @@ resource "azurerm_network_security_group" "main" {
 
 # Create a network interface for VMs and attach the PIP and the NSG
 resource "azurerm_network_interface" "main" {
-  name                      = "myNIC"
+  name                      = "NIC"
   location                  = "${azurerm_resource_group.main.location}"
   resource_group_name       = "${azurerm_resource_group.main.name}"
-  #network_security_group_id = "${azurerm_network_security_group.main.id}"
+  network_security_group_id = "${azurerm_network_security_group.main.id}"
 
   ip_configuration {
     name                          = "nicconfig"
     subnet_id                     = "${azurerm_subnet.internal.id}"
     private_ip_address_allocation = "static"
-    private_ip_address            = "${cidrhost("10.100.1.8/24", 4)}"
+    private_ip_address            = "${cidrhost("10.100.2.16/24", 4)}"
   }
 }
 
 # Create a new Virtual Machine based on the Golden Image
 resource "azurerm_virtual_machine" "vm" {
-  name                             = "AZLXSPTDEVOPS01"
+  name                             = "AZLXDEVOPS01"
   location                         = "${azurerm_resource_group.main.location}"
   resource_group_name              = "${azurerm_resource_group.main.name}"
   network_interface_ids            = ["${azurerm_network_interface.main.id}"]
@@ -86,11 +78,11 @@ resource "azurerm_virtual_machine" "vm" {
   delete_data_disks_on_termination = true
 
   storage_image_reference {
-    id = "${data.azurerm_image.img.id}"
+    id = "${data.azurerm_image.search.id}"
   }
 
   storage_os_disk {
-    name              = "AZLXSPTDEVOPS01-OS"
+    name              = "AZLXDEVOPS01-OS"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
@@ -98,11 +90,8 @@ resource "azurerm_virtual_machine" "vm" {
 
   os_profile {
     computer_name  = "APPVM"
-    admin_username = "admin"
-    admin_password = "admin#2019"
+    admin_username = "Ratan"
+    admin_password = "Rudra0@gmail"
   }
 
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
 }
